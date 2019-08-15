da381fdae31f86e2611a1096d2bb4f110876043f
mathlyml <38436197+mathlyml@users.noreply.github.com>
2019-08-15 11:55:08 +0800

    m

--- assets/js/toggle_note.js
+++ assets/js/toggle_note.js
@@ -3,14 +3,12 @@ function toggle_note()
 	if(document.getElementById("note").style.display == "none")
 	{
 		document.getElementById("note").style.display = "inline";
-		document.getElementById("show_note_icon").style.display = "none";
-		document.getElementById("hide_note_icon").style.display = "inline";
+		document.getElementById("show_note_icon").innerHTML = "&#708;";
 		document.getElementById("note_link").title = "Show less";
 	} else
 	{
 		document.getElementById("note").style.display = "none";
-		document.getElementById("show_note_icon").style.display = "inline";
-		document.getElementById("hide_note_icon").style.display = "none";
+		document.getElementById("show_note_icon").innerHTML = "&#709;";
 		document.getElementById("note_link").title = "Show more";
 	}
 }
